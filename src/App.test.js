d6796c4dae8471c884c2a817953a49478a89be73
kayode <kayode.richards@gmail.com>
2020-10-29 22:02:52 -0400

    mvp

--- src/App.test.js
+++ src/App.test.js
@@ -8,9 +8,26 @@ test("renders App without crashing", () => {
   //1. get acces to form fields
   const firstName = screen.getByLabelText(/first name/i)
   const lastName = screen.getByLabelText(/last name/i)
-  const message = screen.getByLabelText(/message/i)
   const email = screen.getByLabelText(/email/i)
 
+  //2.add text to fields and expect
 
+  expect(firstName.value).toEqual('')
+  fireEvent.change(firstName, {target : {value: 'Kay'}})
+  expect(firstName.value).not.toEqual('')
+  ////////////////////////////////////////////////////////////////
+
+  expect(lastName.value).toEqual('')
+  fireEvent.change(lastName, {target: {value: 'richards'}})
+  expect(lastName.value).not.toEqual('')
+  ////////////////////////////////////////////////////////////
+
+
+  expect(email.value).toEqual('')
+  fireEvent.change(email, {target: {value: 'email@email.com'}})
+  expect(email.value).not.toEqual('')
+  ///////////////////////////////////////////////////////////////////
+
+  
 
 });
