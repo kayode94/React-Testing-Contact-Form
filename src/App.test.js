9be2be12e13e87c23338a47c55ef076396a153e0
kayode <kayode.richards@gmail.com>
2020-10-29 21:49:13 -0400

    getting started

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,16 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
 test("renders App without crashing", () => {
   render(<App />);
+
+  //1. get acces to form fields
+  const firstName = screen.getByLabelText(/first name/i)
+  const lastName = screen.getByLabelText(/last name/i)
+  const message = screen.getByLabelText(/message/i)
+  const email = screen.getByLabelText(/email/i)
+
+
+
 });
